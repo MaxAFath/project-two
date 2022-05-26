25bd516b4307bbc5422176e45b6899075319d98c
VerbalDye <dhunts258@gmail.com>
2022-05-25 20:42:30 -0400

    added routes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // server packages
 const express = require('express');
-// const routes = require('./controllers');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 
@@ -33,7 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.engine('handlebars', hbs.engine);
 // app.set('view engine', 'handlebars');
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
