8dc3d8a504f12d40baeba274de94688fc672cdfe
VerbalDye <dhunts258@gmail.com>
2022-05-30 19:25:34 -0400

    added fileupload package

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
 // session packages
 const session = require('express-session');
@@ -30,8 +31,10 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(fileUpload());
 
 app.use(routes);
 
