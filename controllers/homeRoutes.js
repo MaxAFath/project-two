c54e6b4332d42abf1721dc585b5ad8671a5f036f
VerbalDye <dhunts258@gmail.com>
2022-05-31 19:44:35 -0400

    added single product page and route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -10,14 +10,14 @@ router.get('/', (req, res) => {
             attributes: ['username']
         }]
     })
-    .then(dbPorductData => {
-        const products = dbPorductData.map(product => product.get({ plain: true }));
-        res.render('homepage', { products, loggedIn: req.session.loggedIn });
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then(dbPorductData => {
+            const products = dbPorductData.map(product => product.get({ plain: true }));
+            res.render('homepage', { products, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 router.get('/login', (req, res) => {
@@ -39,8 +39,34 @@ router.get('/signup', (req, res) => {
 })
 
 router.get('/product/:id', (req, res) => {
-    
-    res.render('product', {});
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'name', 'description', 'created_at', 'price'],
+        include: [
+            {
+                model: Image,
+                attributes: ['id', 'file_name']
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbProductData => {
+            if (!dbProductData) {
+                res.status(404).json({ message: 'Product not found' });
+                return;
+            }
+            const product = dbProductData.get({ plain: true });
+            res.render('single-product', { product, loggedIn: req.session.loggedIn });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
