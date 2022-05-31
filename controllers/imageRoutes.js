e453de8c4b677a4eb67d033059a9e3edab7320c9
VerbalDye <dhunts258@gmail.com>
2022-05-30 20:26:37 -0400

    added image api functionality

--- controllers/imageRoutes.js
+++ controllers/imageRoutes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { Image } = require('../models');
+const path = require('path');
+const { dirname } = require('path');
 
 router.use('/:id', (req, res) => {
     Image.findOne({
@@ -7,8 +9,11 @@ router.use('/:id', (req, res) => {
             id: req.params.id
         }
     }).then(dbImageData => {
-        const file_url = '/images/' + dbImageData.file_name;
-        res.sendFile(file_url);
+        const fileName = path.resolve(__dirname+'/../images/' + dbImageData.file_name);
+        res.sendFile(fileName);
+    }).catch(err => {
+        console.log(err);
+        res.status(400).json(err);
     })
 })
 
