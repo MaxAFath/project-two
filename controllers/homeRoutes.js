7e44f8cd7b2b6aadf7ae7fb3b3c2bbc15f9fd02c
VerbalDye <dhunts258@gmail.com>
2022-05-31 19:36:40 -0400

    added variables to homeroutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,22 @@ const router = require("express").Router();
 const { Product, User, Image } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {});
+    Product.findAll({
+        attributes: ['id', 'name', 'create_at'],
+        order: [['created_at', 'DESC']],
+        include: [{
+            model: User,
+            attributes: ['username']
+        }]
+    })
+    .then(dbPorductData => {
+        const products = dbPorductData.map(product => product.get({ plain: true }));
+        res.render('homepage', { products, loggedIn: req.session.loggedIn });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.get('/product', (req, res) => {
