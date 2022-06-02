6d4b542a2b2a4af5313f487e2b424861c6a2656e
VerbalDye <dhunts258@gmail.com>
2022-06-01 20:03:08 -0400

    added purchases

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,7 +1,21 @@
 const withAuth = require('../utils/auth');
 const router = require('express').Router();
+const { Product, Purchase } = require('../models');
 
-router.get('/', withAuth, (req, res) => {
+router.get('/', withAuth, async (req, res) => {
+    const dbProductData = Product.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+    });
+
+    const dbPurchaseData = Purchase.findAll({
+        where: {
+            user_id: req.session.user_id
+        }
+    });
+
+    
     res.render('dashboard', {});
 });
 
